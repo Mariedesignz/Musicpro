07513c443485bf900fd0d358379d5c29898589e5
Marie Designz (Tina Marie) <55719730+Mariedesignz@users.noreply.github.com>
2021-02-22 20:14:19 -0500

    harrypotter

---
+++ harry.py
@@ -0,0 +1,12 @@
+import random
+
+number = random.randint(1,4)
+
+if number ==1:
+    print("Ravenclaw")
+elif number ==2:
+    print("Hufflepuff")
+elif number ==3:
+    print("Slytherin")
+else:
+    print("Gryffindor")
