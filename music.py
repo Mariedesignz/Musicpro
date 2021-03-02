203a455f31b05c7c3a06b47719c35e41d8ff3a51
Marie Designz (Tina Marie) <55719730+Mariedesignz@users.noreply.github.com>
2021-03-01 19:57:17 -0500

    api commit

--- music.py
+++ music.py
@@ -1,1 +1,16 @@
-Hello World
+import http.client
+
+conn = http.client.HTTPSConnection("deezerdevs-deezer.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "bcd47eac68mshe3ce8d7db0f5cf2p1cbc65jsn4367b30fca5b",
+    'x-rapidapi-host': "deezerdevs-deezer.p.rapidapi.com"
+    }
+
+conn.request("GET", "/search?q=Jazz", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
+#This code leads too my Dezzer api. This Api is used too pull muisc from a database.
